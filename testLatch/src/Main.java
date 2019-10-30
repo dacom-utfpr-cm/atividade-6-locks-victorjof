8180e0838843a14ac06ac714b79e8b6233d57d25
Victor Figueira <victorjosefigueira@gmail.com>
2019-10-29 23:05:05 -0300

    Clean

--- testLatch/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        double [] a = new double[]{5,0,5};
-        Stencil stencil = new Stencil(a,1000000);
-        stencil.iterate();
-    }
-}
